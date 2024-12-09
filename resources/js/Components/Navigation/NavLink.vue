8475ca66944039b904fa820dc470d072b8a2961e
Aviran Abramov <aviranabramov@gmail.com>
2024-12-09 18:02:19 +0200

    Refactor: improve NavLink component

--- resources/js/Components/Navigation/NavLink.vue
+++ resources/js/Components/Navigation/NavLink.vue
@@ -2,18 +2,27 @@
 import { Link } from '@inertiajs/vue3';
 import { computed } from 'vue';
 
-defineProps({
-    href: String,
-    active: Boolean
+const props = defineProps({
+    href: {
+        type: String,
+        required: true,
+    },
+    active: {
+        type: Boolean,
+        required: true,
+    }
+})
+
+const classes = computed(() => {
+    const defaultClasses = 'rounded-md px-3 py-2 text-sm font-medium';
+    const activeClasses = props.active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white';
+
+    return `${defaultClasses} ${activeClasses}`;
 });
 </script>
 
 <template>
-    <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-    <Link
-        :href="href"
-        :class="['rounded-md px-3 py-2 text-sm font-medium', active ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white']"
-    >
+    <Link :href="href" :class="classes">
         <slot />
     </Link>
 </template>
