04cb0aeaa558ba9305e0b674fefd3ff9e8216034
Aviran Abramov <aviranabramov@gmail.com>
2024-12-10 21:54:23 +0200

    Use Vue 3.5 reactive prop destructure in NavLink

--- resources/js/Components/Navigation/NavLink.vue
+++ resources/js/Components/Navigation/NavLink.vue
@@ -2,14 +2,14 @@
 import { Link } from '@inertiajs/vue3';
 import { computed } from 'vue';
 
-const props = defineProps<{
+const { href, active } = defineProps<{
     href: string,
     active: boolean
 }>();
 
 const classes = computed(() => {
     const baseClasses = 'rounded-md px-3 py-2 text-sm font-medium';
-    const activeClasses = props.active
+    const activeClasses = active
         ? 'bg-gray-900 text-white'
         : 'text-gray-300 hover:bg-gray-700 hover:text-white';
 
@@ -21,7 +21,7 @@ const classes = computed(() => {
     <Link
         :href
         :class="classes"
-        :aria-current="props.active ? 'page' : null"
+        :aria-current="active ? 'page' : null"
     >
         <slot />
     </Link>
