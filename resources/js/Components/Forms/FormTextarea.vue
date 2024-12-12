09f2a4eb2d08559e248787fc06c62decea923303
Aviran Abramov <aviranabramov@gmail.com>
2024-12-12 20:14:22 +0200

    Replace FormInput with FormTextarea in sup/create

---
+++ resources/js/Components/Forms/FormTextarea.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+
+const model = defineModel<string|null>();
+
+interface IFormTextarea {
+    id: string;
+    name: string;
+    placeholder: string;
+    rows: string;
+}
+
+const { id, name, placeholder, rows } = defineProps<IFormTextarea>();
+</script>
+
+<template>
+<textarea
+    v-model="model"
+    :id
+    :name
+    :placeholder
+    :rows
+    class="w-80 rounded outline outline-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-green-400"></textarea>
+</template>
--- resources/js/Pages/Supplements/Create.vue
+++ resources/js/Pages/Supplements/Create.vue
@@ -3,6 +3,7 @@ import FormError from '@/Components/Forms/FormError.vue';
 import FormInput from '@/Components/Forms/FormInput.vue';
 import FormLabel from '@/Components/Forms/FormLabel.vue';
 import FormSubmitButton from '@/Components/Forms/FormSubmitButton.vue';
+import FormTextarea from '@/Components/Forms/FormTextarea.vue';
 import { ISupplement } from '@/interfaces/supplements';
 import AppLayout from '@/Layouts/AppLayout.vue';
 import { Head, useForm } from '@inertiajs/vue3';
@@ -22,7 +23,7 @@ const form = useForm<ISupplement>({
         <form @submit.prevent="form.post(route('supplements.store'))" class="space-y-4 border border-black rounded w-[400px] mx-auto py-4 flex flex-col items-center">
             <!-- Name -->
             <div>
-                <FormLabel htmlFor="name">Name</FormLabel>
+                <FormLabel htmlFor="name">Supplement Name</FormLabel>
 
                 <FormInput
                     v-model="form.name"
@@ -39,12 +40,12 @@ const form = useForm<ISupplement>({
             <div>
                 <FormLabel htmlFor="description">Description</FormLabel>
 
-                <FormInput
+                <FormTextarea
                     v-model="form.description"
-                    type="text"
-                    name="description"
                     id="description"
+                    name="description"
                     placeholder="Herb that promotes relaxation, helps with mental clarity, reduces stress and cortisol levels."
+                    rows="3"
                 />
 
                 <FormError v-if="form.errors.description">{{ form.errors.description }}</FormError>
@@ -54,12 +55,12 @@ const form = useForm<ISupplement>({
             <div>
                 <FormLabel htmlFor="description">Additional Info 1</FormLabel>
 
-                <FormInput
+                <FormTextarea
                     v-model="form.additional_info_1"
-                    type="text"
-                    name="additional_info_1"
                     id="additional_info_1"
+                    name="additional_info_1"
                     placeholder="Ashwagandha is an adaptogenic herb used in Ayurvedic medicine to help the body adapt to stress and improve overall resilience."
+                    rows="4"
                 />
 
                 <FormError v-if="form.errors.additional_info_1">{{ form.errors.additional_info_1 }}</FormError>
@@ -69,12 +70,12 @@ const form = useForm<ISupplement>({
             <div>
                 <FormLabel htmlFor="description">Additional Info 2</FormLabel>
 
-                <FormInput
+                <FormTextarea
                     v-model="form.additional_info_2"
-                    type="text"
-                    name="additional_info_2"
                     id="additional_info_2"
+                    name="additional_info_2"
                     placeholder="Studies suggest regular use may enhance sleep quality, support cognitive function, and balance mood by regulating stress hormones."
+                    rows="4"
                 />
 
                 <FormError v-if="form.errors.additional_info_2">{{ form.errors.additional_info_2 }}</FormError>
