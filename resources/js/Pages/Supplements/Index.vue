6bba5d9fbca95f03ace9a4ffbb39cb4062e554cc
Aviran Abramov <aviranabramov@gmail.com>
2024-12-12 14:43:56 +0200

    Create simple UI for supplement cards

--- resources/js/Pages/Supplements/Index.vue
+++ resources/js/Pages/Supplements/Index.vue
@@ -12,6 +12,17 @@ const { supplements } = defineProps<{
     <Head title="Supplements List" />
 
     <AppLayout title="Supplements List">
-        
+        <section v-if="supplements">
+            <!-- Supplements Container -->
+            <div class="flex flex-wrap justify-center gap-4">
+                <!-- Supplement Card -->
+                <div v-for="supplement in supplements" :key="supplement.name" class="border rounded border-green-700 p-2 w-[300px]">
+                    <h3 class="text-lg font-semibold">{{ supplement.name }}</h3>
+                    <p class="mt-0.5 text-gray-600 text-sm">{{ supplement.description }}</p>
+                </div>
+            </div>
+        </section>
+
+        <p v-else>No supplements found yet.</p>
     </AppLayout>
 </template>
---
+++ resources/js/interfaces/supplements.d.ts
@@ -0,0 +1,6 @@
+export interface ISupplement {
+    name: string;
+    description: string;
+    additional_info_1: string;
+    additional_info_2: string;
+}
