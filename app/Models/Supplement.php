4d53962a4c4feec3ca87662d29429f6e8eef9ed2
Aviran Abramov <aviranabramov@gmail.com>
2024-12-12 18:21:15 +0200

    Capitalize all inputs for new supplement

--- app/Models/Supplement.php
+++ app/Models/Supplement.php
@@ -3,8 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Supplement extends Model
 {
     protected $fillable = ['name', 'description', 'additional_info_1', 'additional_info_2'];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = Str::ucfirst($value);
+    }
+
+    public function setDescriptionAttribute($value)
+    {
+        $this->attributes['description'] = Str::ucfirst($value);
+    }
+
+    public function setAdditionalInfo1Attribute($value)
+    {
+        $this->attributes['additional_info_1'] = Str::ucfirst($value);
+    }
+
+    public function setAdditionalInfo2Attribute($value)
+    {
+        $this->attributes['additional_info_2'] = Str::ucfirst($value);
+    }
 }
