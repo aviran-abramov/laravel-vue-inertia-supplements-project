dec2088f37aeddfe54188578c34341aceb199be4
Aviran Abramov <aviranabramov@gmail.com>
2024-12-16 17:44:10 +0200

    Refactor: improve FlashMessage colors

--- resources/js/Components/FlashMessage.vue
+++ resources/js/Components/FlashMessage.vue
@@ -1,8 +1,8 @@
 <template>
-    <div role="alert" class="alert border border-black mb-6 bg-green-500 mx-auto max-w-[920px]">
+    <div role="alert" class="alert border border-green-600 mb-6 bg-white mx-auto max-w-[920px]">
         <svg
             xmlns="http://www.w3.org/2000/svg"
-            class="h-6 w-6 shrink-0 stroke-current"
+            class="h-6 w-6 shrink-0 stroke-current text-green-600"
             fill="none"
             viewBox="0 0 24 24"
         >
@@ -12,7 +12,7 @@
             stroke-width="2"
             d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
         </svg>
-        <span class="font-bold">
+        <span class="font-bold text-green-600">
             <slot />
         </span>
     </div>
